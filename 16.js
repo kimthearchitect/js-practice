5f36e5e74e9ff4ae9a698c80ef9047b66d624249
Tomohisa Kimura <tomohisakimura@TomohisanoMacBook-puro.local>
2020-05-25 09:55:51 +0900

    Done Q16 use slice to get array element

---
+++ 16.js
@@ -0,0 +1,8 @@
+//Q.16 配列から要素を取り出す（slice）
+// const array = ['a1','a2','a3','a4','a5']
+// のインデックス2〜4の要素を取り出し、 配列として出力しなさい。 実行された後のarrayの要素を教えてください
+
+const array = ['a1', 'a2', 'a3', 'a4', 'a5']
+const afterArray = array.slice(2, 5)
+
+console.log(afterArray)
