4beb7ba2b2f45c1240f661794d55c0b091b2a63d
Tomohisa Kimura <tomohisakimura@TomohisanoMacBook-puro.local>
2020-05-28 15:46:44 +0900

    Q28 add use preventExtenxions

--- 28.js
+++ 28.js
@@ -2,6 +2,27 @@
 //let obj = {x : 2, y: 3};
 //このobjをプロパティ追加不可、削除変更は可能にし、プロパティ追加不可か否かの判定メソッドでtrueが返る事を確認した後、objのkeyを列挙してください。
 
+let test = { x: 2, y: 3 }
+
+//プロパティの追加のみを不可能にするメソッド preventExtensions
+Object.preventExtensions(test)
+console.log(test)
+
+//プロパティが追加されていないか確認
+console.log(Object.isExtensible(test))
+//判定でfalseなので追加不可
+test.z = 5
+console.log(test)
+
+//オブジェクトのkeyを列挙
+console.log(Object.keys(test))
+
+
+
+//preventExtensionsとfreezeの違い
+//http://unageanu.hatenablog.com/entry/20110918/1316320274
+
+
 let obj = { x: 2, y: 3 }
 
 //freezeを使ってオブジェクトを凍結させる
@@ -14,8 +35,8 @@ console.log(obj)
 console.log(Object.isFrozen(obj))
 //trueなので凍結している
 
-//
 
+//浅い凍結
 const shallowFreeze = {
   value: {},
   item: 'bag'
