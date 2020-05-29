8784adcf0f12d45340eb51d5dbf1a7fb8f58f4c7
Tomohisa Kimura <tomohisakimura@TomohisanoMacBook-puro.local>
2020-05-29 12:02:20 +0900

    fix const name

--- 49.js
+++ 49.js
@@ -1,8 +1,8 @@
 //Q.49 getメソッド
 //[['one', 'info@fa'],['two', 'send@fafa'],['three', 'hoso@fafa']];の'two'の値を取得してください
 
-const arr = [['one', 'info@fa'], ['two', 'send@fafa'], ['three', 'hoso@fafa']]
-const myMap = new Map(arr)
+const fafa = [['one', 'info@fa'], ['two', 'send@fafa'], ['three', 'hoso@fafa']]
+const myMap = new Map(fafa)
 const two = myMap.get('two')
 console.log(two)
 
